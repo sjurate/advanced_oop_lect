bb52a5296dda8c0201e4975fd720a4af721901f4
Mantas Boronilščikovas <mantas.boronilscikovas@swedbank.lt>
2022-03-07 17:36:35 +0200

    initial

---
+++ src/main/java/com/academy/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.academy.controller;
+
+import com.academy.entity.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    public MainController() {
+        
+    }
+
+    @RequestMapping("/customer/all")
+    public List<Customer> fetchCustomers() {
+        return null;
+    }
+
+    @PostMapping("/customer/insert")
+    public void insertCustomer(@RequestBody Customer customer) {
+    }
+
+    @DeleteMapping("/customer/delete/{customerId}")
+    public void deleteCustomer(@PathVariable String customerId) {
+    }
+}
